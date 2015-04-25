c557c52ca1fab6d336046a2867567ba2c7c8bc97
Jui-Fa Tsai <jui-fa.tsai@xern.ch>
2015-04-25 16:33:08 +0200

    Add many function for creating new sections or options

--- elcomandanteGUI/parameters/elComandante_conf.py
+++ elcomandanteGUI/parameters/elComandante_conf.py
@@ -15,7 +15,18 @@ class elComandante_conf:
 		self.debug = debug	
 
 		# Pars list
-		self.list_Sections=["Directories", "TestboardAddress", "defaultParameters", "subsystem", "jumoClient", "keithleyClient", "lowVoltageClient", "xrayClient", "psiClient", "Transfer"]
+		self.list_Sections = [
+			"Directories", 
+			"TestboardAddress", 
+			"defaultParameters", 
+			"subsystem", 
+			"jumoClient", 
+			"keithleyClient", 
+			"lowVoltageClient", 
+			"xrayClient", 
+			"psiClient", 
+			"Transfer"
+		]
 		self.list_Directories=["baseDir", "testDefinitions", "moduleDB", "subserverDir", "dataDir", "jumoDir", "keithleyDir", "defaultParameters", "scriptDir"]
 		self.list_TestboardAddress=["TB0", "TB1", "TB2", "TB3"]
 		self.list_defaultParameters=["Roc", "Full"]
@@ -39,30 +50,17 @@ class elComandante_conf:
 		self.psiClient = {}
 		self.Transfer = {}
 		self.Sections = {
-			"Directories":self.Directories,
-			"TestboardAddress":self.TestboardAddress,
-			"defaultParameters":self.defaultParameters,
-			"subsystem":self.subsystem,
-			"jumoClient":self.jumoClient,
-			"keithleyClient":self.keithleyClient,
-			"lowVoltageClient":self.lowVoltageClient,
-			"xrayClient":self.xrayClient,
-			"psiClient":self.psiClient,
-			"Transfer":self.Transfer
+			self.list_Sections[0]:self.Directories,
+			self.list_Sections[1]:self.TestboardAddress,
+			self.list_Sections[2]:self.defaultParameters,
+			self.list_Sections[3]:self.subsystem,
+			self.list_Sections[4]:self.jumoClient,
+			self.list_Sections[5]:self.keithleyClient,
+			self.list_Sections[6]:self.lowVoltageClient,
+			self.list_Sections[7]:self.xrayClient,
+			self.list_Sections[8]:self.psiClient,
+			self.list_Sections[9]:self.Transfer
 		}
-	
-	def getDefault(self, elComandante_conf_default):
-		self.loadDefault(elComandante_conf_default)
-		self.fill("Directories",       self.list_Directories,       self.Directories )
-		self.fill("TestboardAddress",  self.list_TestboardAddress,  self.TestboardAddress )
-		self.fill("defaultParameters", self.list_defaultParameters, self.defaultParameters )
-		self.fill("subsystem",         self.list_subsystem,         self.subsystem )
-		self.fill("jumoClient",        self.list_jumoClient,        self.jumoClient )
-		self.fill("keithleyClient",    self.list_keithleyClient,    self.keithleyClient )
-		self.fill("lowVoltageClient",  self.list_lowVoltageClient,  self.lowVoltageClient )
-		self.fill("xrayClient",        self.list_xrayClient,        self.xrayClient )
-		self.fill("psiClient",         self.list_psiClient,         self.psiClient )
-		self.fill("Transfer",          self.list_Transfer,          self.Transfer )
 
 	def loadDefault(self, elComandante_conf_default):
 		self.defaultConf = elComandante_conf_default
@@ -73,6 +71,7 @@ class elComandante_conf:
 			print '>> [INFO] Reading %s...' %self.defaultConf
 			self.parser.read(self.defaultConf)
 			self.hasDefault = True 
+		return
 
 	def fill(self, section, options, output ):
 		if self.hasDefault:
@@ -86,14 +85,104 @@ class elComandante_conf:
 		else:
 			print ">> [ERROR] Please do elComandante_conf.loadDault(file) first." 
 			sys.exit()
+		return
 
-	def printConfig(self):
+	def getDefault(self, elComandante_conf_default):
+		self.loadDefault(elComandante_conf_default)
+		self.fill("Directories",       self.list_Directories,       self.Directories )
+		self.fill("TestboardAddress",  self.list_TestboardAddress,  self.TestboardAddress )
+		self.fill("defaultParameters", self.list_defaultParameters, self.defaultParameters )
+		self.fill("subsystem",         self.list_subsystem,         self.subsystem )
+		self.fill("jumoClient",        self.list_jumoClient,        self.jumoClient )
+		self.fill("keithleyClient",    self.list_keithleyClient,    self.keithleyClient )
+		self.fill("lowVoltageClient",  self.list_lowVoltageClient,  self.lowVoltageClient )
+		self.fill("xrayClient",        self.list_xrayClient,        self.xrayClient )
+		self.fill("psiClient",         self.list_psiClient,         self.psiClient )
+		self.fill("Transfer",          self.list_Transfer,          self.Transfer )
+		return
+
+	def listSections(self):
+		print ">> [INFO] Call elComandante_conf::listSections()"	
+		print ">>        Sections: "
+		for sec in self.list_Sections:
+			print ">>                  "+sec
+		return
+	
+	def makeNewSection(self, newSec, newOpts={} ):
+		if self.debug:  
+			print ">> [DEBUG] Call elComandante_conf::makeNewSection( newSec, newOpts  )"	
+			print ">>         newSec: "+newSec	
+			print ">>         newOpts: "+str(newOpts)
+		if newSec in self.Sections:
+			print ">> [ERROR] Section "+newSec+" existed"
+			return
+		self.list_Sections.append(newSec)
+		self.Sections[newSec]=newOpts
+		return
+
+	def makeNewOption(self, secName, newOptsName, newValue="" ):
+		if self.debug:  
+			print ">> [DEBUG] Call elComandante_conf::makeNewOption( secName, newOptsName, newValue  )"	
+			print ">>         secName:     "+secName	
+			print ">>         newOptsName: "+newOptsName
+			print ">>         newValue:    "+newValue
+		if secName in self.Sections:
+			if newOptsName in self.Sections[secName]:
+				print ">> [ERROR] Option "+newOptsName+" existed in "+secName
+				return
+			else:
+				self.Sections[secName][newOptsName]=newValue
+		else:
+			print ">> [ERROR] Not found section "+secName
+		return
+		
+	def changeOptValue(self, secName, optName, newValue ):
+		if self.debug:  
+			print ">> [DEBUG] Call elComandante_conf::changeOptValue( secName, optName, newValue  )"	
+			print ">>         secName:  "+secName	
+			print ">>         optName:  "+optName
+			print ">>         newValue: "+newValue
+		if secName in self.Sections:
+			if optName in self.Sections[secName]:
+				self.Sections[secName][optName]=newValue
+			else:
+				print ">> [ERROR] Not found option "+optName+" in "+secName
+		else:
+			print ">> [ERROR] Not found section "+secName
+		return
+
+	def listOptions(self, secName ):
+		if secName in self.Sections:
+			print ">> [INFO] Call elComandante_conf::listOptions( "+secName+" )" 
+			print ">>        Options: "
+			for opt in self.Sections[secName]:
+				print ">>                "+opt
+		else:
+			print ">> [INFO] Call elComandante_conf::listOptions( "+secName+" )" 
+			print ">> [ERROR] Not found section "+secName
+		return
+
+	def callOption(self, secName, optName ):
+		if secName in self.Sections:
+			if optName in self.Sections[secName]:
+				print ">> [INFO] Call elComandante_conf::callOption( "+secName+", "+optName+" )" 
+				print ">>        Value: "+self.Sections[secName][optName]
+			else:
+				print ">> [INFO] Call elComandante_conf::callOption( "+secName+", "+optName+" )" 
+				print ">> [ERROR] Not found option "+optName+" in "+secName
+		else:
+			print ">> [INFO] Call elComandante_conf::callOption( "+secName+", "+optName+" )" 
+			print ">> [ERROR] Not found section "+secName
+		return
+
+	def callConfig(self):
 		print "\n############ Current elComandante.conf #################\n"
 		for section in self.list_Sections:
 			print " ["+section+"]"
 			for opt in sorted(self.Sections[section]): 
 				print " "+opt+" : "+self.Sections[section][opt] 
 			print "\n"
+		return
 
 	def makeConfig(self, outFile="./elComandante.conf" ):
 		print ">> [INFO] Writing conf into "+outFile+"..."
@@ -103,11 +192,31 @@ class elComandante_conf:
 			for opt in sorted(self.Sections[section]): 
 				output.write(opt+" : "+self.Sections[section][opt]+"\n") 
 			output.write("\n")
-		print ">> [INFO] Done"
+		return ">> [INFO] Done"
 		
 
-################ test ################
+################ example ################
 #elini = elComandante_conf(debug=True)
+##elini = elComandante_conf()
 #elini.getDefault("../elComandante.conf")
-#elini.printConfig()
+#elini.listSections()
+#elini.listOptions("mySection")
+#newopts = {"Opt1":""}
+#elini.makeNewSection("mySection1", newopts)
+#elini.makeNewSection("mySection1")
+#elini.makeNewSection("mySection2")
+#elini.listSections()
+#elini.listOptions("mySection")
+#elini.listOptions("mySection1")
+#elini.listOptions("mySection2")
+#elini.makeNewOption("mySection1", "Opt1", "Here")
+#elini.makeNewOption("mySection1", "Opt2", "Here")
+#elini.makeNewOption("mySection2", "Opt1", "Here")
+#elini.listOptions("Transfer")
+#elini.callOption("Transfer", "user" )
+#elini.changeOptValue("mySection1", "Opt3", "There" )
+#elini.changeOptValue("mySection1", "Opt1", "There" )
+#elini.changeOptValue("Transfer", "user", "jtsai" )
+#elini.callOption("Transfer", "user" )
+#elini.callConfig()
 #elini.makeConfig()
