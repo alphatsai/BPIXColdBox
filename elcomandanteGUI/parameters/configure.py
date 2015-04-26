32364e3471213fe0c07315488c9b956768566c9e
Jui-Fa Tsai <jui-fa.tsai@xern.ch>
2015-04-26 21:26:15 +0200

    Change the elComandante_conf.py inheritance from configure.py

---
+++ elcomandanteGUI/parameters/configure.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+import os, re, sys, shutil
+import math, ROOT
+
+#sys.path.insert(1,os.path.dirname(os.path.abspath(__file__))+'/../')
+from beautyLine import *
+from ConfigParser import *
+
+class configure:
+
+	def __init__(self, debug=False):
+		self.className = "configure"
+		self.defaultOutput = "config"
+		self.defaultConf = ""
+		self.parser = SafeConfigParser()
+		self.parser.optionxform = str
+		self.hasDefault = False
+		self.debug = debug	
+
+		# list: Pars list -  can be customily extended by fuctions 
+		self.list_Sections = [
+			# section_name,
+		]
+
+		# dict: Default sections and options in configure file
+		self.list_Default = {
+			# section_name:["option1", "option2", ...],
+		}
+
+		# dict: Pars map - default containor
+		self.Sections = {
+			# section_name:{"option1":"value1", "option2":"value2", ... },
+		}
+
+	def loadDefault( self, configFile="" ):
+		self.defaultConf = configFile
+		if not os.path.isfile(self.defaultConf):
+			print ">> [ERROR] Can't find '"+self.defaultConf+"', or it's not a file..."
+		else: 
+			self.parser.read(self.defaultConf)
+			self.hasDefault = True 
+			print '>> [INFO] Loaded %s...' %self.defaultConf
+		return
+
+	# It will load default sections and options from configure file default structure,
+	# If you want use new section and options in configure file, then "addNewSection" has to be "True"
+	def getDefault(self, configFile, addNewSection=False ):
+		self.loadDefault(configFile)
+		print '>> [INFO] Checking default parameter...' 
+		for defaultSection in self.list_Default:
+			if not self.parser.has_section(defaultSection):
+				print ">> [ERROR] No default section: '"+defaultSection+"' in "+configFile
+				print ">>         Teminated %s::getDefault()" % self.className
+				return
+			else:
+				for defaultOpt in self.list_Default[defaultSection]:
+					if not self.parser.has_option( defaultSection, defaultOpt): 
+						print ">> [ERROR] No default option: '"+defaultOpt+"' under "+defaultSection+" in "+configFile
+						print ">>         Teminated %s::getDefault()"% self.className
+						return
+		print '>> [INFO] Filling parameters...' 
+		for loadSection in self.parser.sections():
+			if loadSection in self.list_Default:
+				for loadOpt in self.parser.options(loadSection):
+					self.Sections[loadSection][loadOpt]=self.parser.get(loadSection, loadOpt)
+					if self.debug: 
+						print ">> [DEBUG] Got default {0:<15s} option: {1:<25s}".format("'"+loadSection+"'", loadOpt)
+			else: 	
+				if addNewSection:
+					self.makeNewSection(loadSection)
+					for loadOpt in self.parser.options(loadSection):
+						self.makeNewOption( loadSection, loadOpt, self.parser.get(loadSection, loadOpt))
+						if self.debug: 
+							print ">> [DEBUG] Got default {0:<15s} option: {1:<25s}".format("'"+loadSection+"'", loadOpt)
+		return
+
+
+	def listSections(self):
+		print ">> [INFO] Call %s::listSections()"% self.className	
+		print ">>        Sections: "
+		for sec in self.list_Sections:
+			print ">>                  "+sec
+		return
+	
+	#def makeNewSection(self, newSec, newOpts={} ):
+	def makeNewSection(self, newSec ):
+		if self.debug:  
+			print ">> [DEBUG] Call %s::makeNewSection( newSec, newOpts  )"% self.className	
+			print ">>         newSec: "+newSec	
+			#print ">>         newOpts: "+str(newOpts)
+		if newSec in self.Sections:
+			print ">> [ERROR] Section "+newSec+" existed"
+			return
+		else:
+			self.list_Sections.append(newSec)
+			self.Sections[newSec]={}
+		return
+
+	def makeNewOption(self, secName="", newOptsName="", newValue="" ):
+		if self.debug:  
+			print ">> [DEBUG] Call %s::makeNewOption( secName, newOptsName, newValue  )"% self.className	
+			print ">>         secName:     "+secName	
+			print ">>         newOptsName: "+newOptsName
+			print ">>         newValue:    "+newValue
+		if secName in self.Sections:
+			if newOptsName in self.Sections[secName]:
+				print ">> [ERROR] Option "+newOptsName+" existed in "+secName
+				return
+			else:
+				self.Sections[secName][newOptsName]=newValue
+		else:
+			print ">> [ERROR] Not found section "+secName
+		return
+		
+	def changeOptValue(self, secName="", optName="", newValue="" ):
+		if self.debug:  
+			print ">> [DEBUG] Call %s::changeOptValue( secName, optName, newValue  )"% self.className	
+			print ">>         secName:  "+secName	
+			print ">>         optName:  "+optName
+			print ">>         newValue: "+newValue
+		if secName in self.Sections:
+			if optName in self.Sections[secName]:
+				self.Sections[secName][optName]=newValue
+			else:
+				print ">> [ERROR] Not found option "+optName+" in "+secName
+		else:
+			print ">> [ERROR] Not found section "+secName
+		return
+
+	def listOptions(self, secName=None ):
+		if secName in self.Sections:
+			print ">> [INFO] Call %s::listOptions( %s )"%( self.className, secName )
+			print ">>        Options: "
+			for opt in self.Sections[secName]:
+				print ">>                "+opt
+		else:
+			print ">> [INFO] Call %s::listOptions( secName )"% self.className 
+			print ">> [ERROR] Not found section "+secName
+		return
+
+	def callOption(self, secName=None, optName=None ):
+		if secName == None or optName == None:
+			print ">> [ERROR] No input in %s::callOption( secName, optName )"% self.className 
+			return	
+		if secName in self.Sections:
+			if optName in self.Sections[secName]:
+				print ">> [INFO] Call %s::callOption( %s, %s )"%( self.className, secName, optName )
+				print ">>        Value: "+self.Sections[secName][optName]
+			else:
+				print ">> [INFO] Call %s::callOption( secName, optName )"% self.className 
+				print ">> [ERROR] Not found option "+optName+" in "+secName
+		else:
+			print ">> [INFO] Call %s::callOption( %s, %s )"%( self.className, secName, optName )
+			print ">> [ERROR] Not found section "+secName
+		return
+
+	def callConfig(self):
+		print "\n############ Current %s #################\n"% self.defaultOutput
+		for section in self.list_Sections:
+			print " ["+section+"]"
+			for opt in sorted(self.Sections[section]): 
+				print " "+opt+" : "+self.Sections[section][opt] 
+			print "\n"
+		return
+
+	def makeConfig( self, outFile=None ):
+		if outFile == None:
+			outFile=self.defaultOutput	
+		print ">> [INFO] Writing conf into "+outFile+"..."
+		output = open(outFile, 'w')
+		for section in self.list_Sections:
+			output.write("["+section+"]\n")
+			for opt in sorted(self.Sections[section]): 
+				output.write(opt+" : "+self.Sections[section][opt]+"\n") 
+			output.write("\n")
+		return ">> [INFO] Done"
+
--- elcomandanteGUI/parameters/elComandante_conf.py
+++ elcomandanteGUI/parameters/elComandante_conf.py
@@ -3,17 +3,20 @@ import os, re, sys, shutil
 import math, ROOT
 
 #sys.path.insert(1,os.path.dirname(os.path.abspath(__file__))+'/../')
-from beautyLine import *
-from ConfigParser import *
+#from beautyLine import *
+#from ConfigParser import *
+from configure import *
 
-class elComandante_conf:
+class elComandante_conf(configure):
 
 	def __init__(self, debug=False):
+		self.className = "elComandante_conf"
+		self.defaultOutput = "elComandante.conf"
 		self.defaultConf = ""
 		self.parser = SafeConfigParser()
 		self.parser.optionxform = str
 		self.hasDefault = False
-		self.debug = debug	
+		self.debug = debug
 
 		# Pars list -  can be customily extended by fuctions 
 		self.list_Sections = [
@@ -67,144 +70,6 @@ class elComandante_conf:
 			# Can be customily extended by fuction
 		}
 
-	def loadDefault(self, elComandante_conf_default="elComandante.conf.default" ):
-		self.defaultConf = elComandante_conf_default
-		if not os.path.isfile(self.defaultConf):
-			print ">> [ERROR] Can't find "+self.defaultConf+", or it's not a file..."
-		else: 
-			self.parser.read(self.defaultConf)
-			self.hasDefault = True 
-			print '>> [INFO] Loaded %s...' %self.defaultConf
-		return
-
-	# It will load default sections and options from elComandante.conf default structure,
-	# If you want use new section and options in elComandante.conf, then "addNewSection" has to be "True"
-	def getDefault(self, elComandante_conf_default, addNewSection=False ):
-		self.loadDefault(elComandante_conf_default)
-		print '>> [INFO] Checking default parameter...' 
-		for defaultSection in self.list_Default:
-			if not self.parser.has_section(defaultSection):
-				print ">> [ERROR] No default section: '"+defaultSection+"' in "+elComandante_conf_default
-				print ">>         Teminated elComandante_conf::getDefault()"
-				return
-			else:
-				for defaultOpt in self.list_Default[defaultSection]:
-					if not self.parser.has_option( defaultSection, defaultOpt): 
-						print ">> [ERROR] No default option: '"+defaultOpt+"' under "+defaultSection+" in "+elComandante_conf_default
-						print ">>         Teminated elComandante_conf::getDefault()"
-						return
-		print '>> [INFO] Filling parameters...' 
-		for loadSection in self.parser.sections():
-			if loadSection in self.list_Default:
-				for loadOpt in self.parser.options(loadSection):
-					self.Sections[loadSection][loadOpt]=self.parser.get(loadSection, loadOpt)
-					if self.debug: 
-						print ">> [DEBUG] Got default {0:<15s} option: {1:<25s}".format("'"+loadSection+"'", loadOpt)
-			else: 	
-				if addNewSection:
-					self.makeNewSection(loadSection)
-					for loadOpt in self.parser.options(loadSection):
-						self.makeNewOption( loadSection, loadOpt, self.parser.get(loadSection, loadOpt))
-						if self.debug: 
-							print ">> [DEBUG] Got default {0:<15s} option: {1:<25s}".format("'"+loadSection+"'", loadOpt)
-		return
-
-
-	def listSections(self):
-		print ">> [INFO] Call elComandante_conf::listSections()"	
-		print ">>        Sections: "
-		for sec in self.list_Sections:
-			print ">>                  "+sec
-		return
-	
-	#def makeNewSection(self, newSec, newOpts={} ):
-	def makeNewSection(self, newSec ):
-		if self.debug:  
-			print ">> [DEBUG] Call elComandante_conf::makeNewSection( newSec, newOpts  )"	
-			print ">>         newSec: "+newSec	
-			#print ">>         newOpts: "+str(newOpts)
-		if newSec in self.Sections:
-			print ">> [ERROR] Section "+newSec+" existed"
-			return
-		else:
-			self.list_Sections.append(newSec)
-			self.Sections[newSec]={}
-		return
-
-	def makeNewOption(self, secName, newOptsName, newValue="" ):
-		if self.debug:  
-			print ">> [DEBUG] Call elComandante_conf::makeNewOption( secName, newOptsName, newValue  )"	
-			print ">>         secName:     "+secName	
-			print ">>         newOptsName: "+newOptsName
-			print ">>         newValue:    "+newValue
-		if secName in self.Sections:
-			if newOptsName in self.Sections[secName]:
-				print ">> [ERROR] Option "+newOptsName+" existed in "+secName
-				return
-			else:
-				self.Sections[secName][newOptsName]=newValue
-		else:
-			print ">> [ERROR] Not found section "+secName
-		return
-		
-	def changeOptValue(self, secName, optName, newValue ):
-		if self.debug:  
-			print ">> [DEBUG] Call elComandante_conf::changeOptValue( secName, optName, newValue  )"	
-			print ">>         secName:  "+secName	
-			print ">>         optName:  "+optName
-			print ">>         newValue: "+newValue
-		if secName in self.Sections:
-			if optName in self.Sections[secName]:
-				self.Sections[secName][optName]=newValue
-			else:
-				print ">> [ERROR] Not found option "+optName+" in "+secName
-		else:
-			print ">> [ERROR] Not found section "+secName
-		return
-
-	def listOptions(self, secName ):
-		if secName in self.Sections:
-			print ">> [INFO] Call elComandante_conf::listOptions( "+secName+" )" 
-			print ">>        Options: "
-			for opt in self.Sections[secName]:
-				print ">>                "+opt
-		else:
-			print ">> [INFO] Call elComandante_conf::listOptions( "+secName+" )" 
-			print ">> [ERROR] Not found section "+secName
-		return
-
-	def callOption(self, secName, optName ):
-		if secName in self.Sections:
-			if optName in self.Sections[secName]:
-				print ">> [INFO] Call elComandante_conf::callOption( "+secName+", "+optName+" )" 
-				print ">>        Value: "+self.Sections[secName][optName]
-			else:
-				print ">> [INFO] Call elComandante_conf::callOption( "+secName+", "+optName+" )" 
-				print ">> [ERROR] Not found option "+optName+" in "+secName
-		else:
-			print ">> [INFO] Call elComandante_conf::callOption( "+secName+", "+optName+" )" 
-			print ">> [ERROR] Not found section "+secName
-		return
-
-	def callConfig(self):
-		print "\n############ Current elComandante.conf #################\n"
-		for section in self.list_Sections:
-			print " ["+section+"]"
-			for opt in sorted(self.Sections[section]): 
-				print " "+opt+" : "+self.Sections[section][opt] 
-			print "\n"
-		return
-
-	def makeConfig(self, outFile="./elComandante.conf" ):
-		print ">> [INFO] Writing conf into "+outFile+"..."
-		output = open(outFile, 'w')
-		for section in self.list_Sections:
-			output.write("["+section+"]\n")
-			for opt in sorted(self.Sections[section]): 
-				output.write(opt+" : "+self.Sections[section][opt]+"\n") 
-			output.write("\n")
-		return ">> [INFO] Done"
-		
 
 ################ example ################
 #elini = elComandante_conf(debug=True)
