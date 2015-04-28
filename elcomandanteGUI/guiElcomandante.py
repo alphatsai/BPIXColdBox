29420fd14d2eddec32d7d763648f892a1b213959
pixel_dev <pixel_dev@pcpixelcb.(none)>
2015-04-28 18:53:56 +0200

    Building demo for Elcomandante without interction functions

--- elcomandanteGUI/guiElcomandante.py
+++ elcomandanteGUI/guiElcomandante.py
@@ -3,6 +3,7 @@ import os, re, sys, shutil
 import math, ROOT
 
 from Tkinter import *
+import tkFont
 
 sys.path.insert(1,os.path.dirname(os.path.abspath(__file__))+'/../')
 from readConfg import elComandante_ini
@@ -10,14 +11,17 @@ from readConfg import elComandante_conf
 
 BG_framTitle='Gray'
 BG_framConfig='LightBlue1'
-BG_framMain='DarkSlateGray1'
+BG_framMain='wheat1'
 
 class interface():
 	def __init__(self, master=None):
 		self.master = master
-		self.framTitle = Frame(master, bg=BG_framTitle)
-		self.framConfig = Frame(master, bg=BG_framConfig, relief=RAISED, borderwidth=2)
-		self.framMain = Frame(master, bg=BG_framMain, relief=RAISED, borderwidth=2)
+		self.master.title("guiElcomandante")
+		self.master["bg"]=BG_framMain
+		self.framMain =  self.master
+		#self.framMain = Frame(self.master, bg=BG_framMain, relief=RAISED, borderwidth=2)
+		#self.framMain = Frame(self.master, relief=RAISED, borderwidth=2)
+		self.framMain.grid()
 		self.iniClass = None
 		self.nSections = 0
 		self.iniSectionsLabel=[]
@@ -38,33 +42,111 @@ class interface():
 	
 	def createWidgets(self):
 
-		self.framTitle.grid(row=0, column=0)
-		self.title = Label(self.framTitle, bg=BG_framTitle)	
+		self.title = Label(self.framMain, bg=BG_framMain, font=('helvetica', 10, 'bold') )
 		self.title["text"]="elComandante_ini"
-		self.title.grid(row=0, column=0, columnspan=1 )
+		self.title.grid(row=0, column=0, columnspan=6, sticky=NSEW )
+		#self.title.pack(fill=X, side=TOP)
 
-		self.framConfig.grid(row=1, column=0)
-		self.lableConfig = Label(self.framConfig, bg=BG_framConfig)
-		self.lableConfig["text"]="Configure input"	
-		self.lableConfig.grid(row=0, column=0, sticky=E)
+		col=0
+		colmax=6
+		while ( col < colmax ):
+			self.pad = Label(self.framMain, bg=BG_framMain, width=10, height=2)
+			self.pad.grid(row=1, column=col)
+			col+=1
 
-		self.entryConfig = Entry(self.framConfig)
-		self.entryConfig["width"]=10	
-		self.entryConfig.insert(0, self.loadConfig)	
-		self.entryConfig.grid(row=0, column=1)
+		# Config 
+		self.lableConfig = Label(self.framMain, bg=BG_framMain, font=('helvetica', 12,))
+		self.lableConfig["text"]="Input Configure"
+		self.lableConfig.grid(row=1, column=0, columnspan=2)
+
+		self.entryConfig = Entry(self.framMain)
+		self.entryConfig["width"]=30
+		self.entryConfig.insert(0, self.loadConfig)
+		self.entryConfig.grid(row=1, column=2, sticky=W, columnspan=2)
 		
-		self.button1Config = Button(self.framConfig, bg="IndianRed2")
+		self.button1Config = Button(self.framMain, bg="IndianRed2", font=('helvetica', 12, 'bold'))
 		self.button1Config["text"]="ReLoad"
-		self.button1Config.grid(row=0, column=2)
+		self.button1Config.grid(row=1, column=4, sticky=E)
 	
-		self.button2Config = Button(self.framConfig, bg="SpringGreen2")
+		self.button2Config = Button(self.framMain, bg="SpringGreen2", font=('helvetica', 12,'bold'))
 		self.button2Config["text"]="Save"
-		self.button2Config.grid(row=0, column=3)
-
-		self.framMain.grid(row=2, column=0)
-		self.title1Main = Label(self.framMain, bg=BG_framMain)
+		self.button2Config.grid(row=1, column=5)
+		
+		# DTB = ['Modules', 'ModuleType', 'TestboardUse']
+		self.title1Main = Label(self.framMain, bg=BG_framMain, font=('helvetica', 12,'bold'))
 		self.title1Main['text']='DTB setting'
-		self.title1Main.grid(row=0, column=0, columnspan=1)
+		self.title1Main.grid(row=2, column=0, columnspan=6, sticky=NSEW )
+		
+		self.modules = Frame(self.framMain, bg=BG_framMain)
+		self.modules.grid(row=3, column=0, columnspan=2, sticky=N+E)
+		self.modulesTitle = Label(self.modules, bg=BG_framMain, text='Modules', font=('helvetica', 12))
+		self.modulesTitle.grid(row=0, column=0, columnspan=2)
+		irow=1
+		for opt in self.iniClass.list_Default['Modules']:
+			newOptLabel = Label(self.modules, bg=BG_framMain)
+			newOptEntry = Entry(self.modules)
+			newOptLabel["text"] = opt
+			newOptLabel.grid( row=irow, column=0, sticky=E+N )
+			newOptEntry['width'] = 10
+			newOptEntry.insert(0, self.iniClass.Sections['Modules'][opt])
+			newOptEntry.grid( row=irow, column=1, sticky=W+N)
+			irow+=1
+
+
+		self.moduleType = Frame(self.framMain, bg=BG_framMain)
+		self.moduleType.grid(row=3, column=2, columnspan=2, sticky=N)
+		self.moduleTypeTitle = Label(self.moduleType, bg=BG_framMain, text='ModuleType', font=('helvetica', 12))
+		self.moduleTypeTitle.grid(row=0, column=0, columnspan=2)
+		irow=1
+		for opt in self.iniClass.list_Default['ModuleType']:
+			newOptLabel = Label(self.moduleType, bg=BG_framMain)
+			var=StringVar()
+			newOptMenu = OptionMenu(self.moduleType, var, "Full", "Roc" )
+			newOptLabel["text"] = opt
+			newOptLabel.grid( row=irow, column=0, sticky=E+N )
+			newOptMenu['width'] =5 
+			newOptMenu['bg'] = 'DeepPink2' 
+			value=self.iniClass.Sections['ModuleType'][opt]
+			if value !=  "Full" and value != "Roc":
+				print ">> [ERROR] ModuleType "+opt+" has wrong value "+value
+				var.set("Error")
+			elif value=="Full":
+				newOptMenu['bg'] = 'SkyBlue1' 
+				var.set(value)
+			elif value=="Roc":
+				newOptMenu['bg'] = 'DarkOliveGreen1' 
+				var.set(value)
+
+			newOptMenu.grid( row=irow, column=1, sticky=W+N)
+			irow+=1
+
+		self.TestboardUse = Frame(self.framMain, bg=BG_framMain)
+		self.TestboardUse.grid(row=3, column=4, columnspan=2, sticky=N+W)
+		self.TestboardUseTitle = Label(self.TestboardUse, bg=BG_framMain, text='TestboardUse', font=('helvetica', 12))
+		self.TestboardUseTitle.grid(row=0, column=0, columnspan=2)
+		irow=1
+		for opt in self.iniClass.list_Default['TestboardUse']:
+			newOptLabel = Label(self.TestboardUse, bg=BG_framMain)
+			newOptButton = Button(self.TestboardUse)
+			newOptLabel["text"] = opt
+			newOptLabel.grid( row=irow, column=0, sticky=E+N )
+			newOptButton['width'] =5 
+			value=self.iniClass.Sections['TestboardUse'][opt]
+			newOptButton['text']="False"
+			newOptButton['bg']="DarkOrange3"
+			if value !=  "True" and value != "False":
+				print ">> [ERROR] TestboardUse "+opt+" has wrong value "+value
+			elif value == "True":
+				newOptButton['text']=value
+				newOptButton['bg']="green2"
+			newOptButton.grid( row=irow, column=1, sticky=W+N)
+			irow+=1
+
+		# Hardware Setting = ['CoolingBox', 'Keithley', 'LowVoltage', 'Xray']
+		self.title2Main = Label(self.framMain, bg=BG_framMain, font=('helvetica', 12,'bold'))
+		self.title2Main['text']='Hardware setting'
+		self.title2Main.grid(row=4, column=0, columnspan=6, sticky=NSEW )
+
 
 	def loadConfig(self, config=""):
 		self.iniClass = elComandante_ini()
