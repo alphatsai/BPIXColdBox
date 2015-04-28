96a94e4234954ed1409d26a4a0b8b015c3ac6ee6
Jui-Fa Tsai <jui-fa.tsai@xern.ch>
2015-04-28 15:10:16 +0200

    Add new example

---
+++ elcomandanteGUI/example/example_06.py
@@ -0,0 +1,61 @@
+
+#!/usr/bin/python
+
+from Tkinter import *
+import Tkinter
+
+top = Tkinter.Tk()
+
+B1 = Tkinter.Button(top, text ="circle", relief=RAISED,
+                         cursor="circle")
+B2 = Tkinter.Button(top, text ="plus", relief=RAISED,
+                         cursor="plus")
+B1.pack()
+B2.pack()
+top.mainloop()
+
+#"arrow"
+#
+#"circle"
+#
+#"clock"
+#
+#"cross"
+#
+#"dotbox"
+#
+#"exchange"
+#
+#"fleur"
+#
+#"heart"
+#
+#"heart"
+#
+#"man"
+#
+#"mouse"
+#
+#"pirate"
+#
+#"plus"
+#
+#"shuttle"
+#
+#"sizing"
+#
+#"spider"
+#
+#"spraycan"
+#
+#"star"
+#
+#"target"
+#
+#"tcross"
+#
+#"trek"
+#
+#"watch"
+#
+#
